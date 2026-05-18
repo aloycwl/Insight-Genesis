0f7535493470f83a04cd24767650b1a478cf016f
Aloysius Chan <63489563+aloycwl@users.noreply.github.com>
2026-05-18 19:07:11 +0800

    Add Datadog RUM initialization to React app

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { initDatadogRum } from './datadogRUM-init'
+
+initDatadogRum()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
